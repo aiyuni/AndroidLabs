5dc95ccbf68a4512a7c9fa40255bf277ee3da057
Perry Li <perry0406@hotmail.com>
2019-09-08 23:52:24 -0700

    added TipCalculator

---
+++ TipCalculator/app/src/main/java/ca/bcit/tipcalculator/MainActivity.java
@@ -0,0 +1,32 @@
+package ca.bcit.tipcalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onConstraintClick(View v) {
+        Intent i = new Intent(this, ConstraintActivity.class);
+        startActivity(i);
+    }
+
+    public void onLinearClick(View v){
+        Intent i = new Intent(this, LinearActivity.class);
+        startActivity(i);
+    }
+
+    public void onRelativeClick(View v){
+        Intent i = new Intent(this, RelativeActivity.class);
+        startActivity(i);
+    }
+
+}
