a204798d353a0c2b3cfd55552dcc02db852eb0d3
Perry Li <perry0406@hotmail.com>
2019-09-15 11:05:54 -0700

    added goodfoodsmarket lab

---
+++ GoodFoodsMarket/app/src/main/java/com/example/goodfoodsmarket/TopLevelActivity.java
@@ -0,0 +1,34 @@
+package com.example.goodfoodsmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.*;
+import java.util.*;
+public class TopLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setListViews();
+    }
+
+    private void setListViews() {
+        ListView view = findViewById(R.id.mainListView);
+        final List<String> categoryList = new ArrayList<>(Arrays.asList(Food.categories));
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, categoryList);
+        view.setAdapter(arrayAdapter);
+
+        /*view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(TopLevelActivity.this, ProductTypeActivity.class);
+                intent.putExtra("categoryName", categoryList.get(position));
+                startActivity(intent);
+            }
+        }); */
+    }
+}
