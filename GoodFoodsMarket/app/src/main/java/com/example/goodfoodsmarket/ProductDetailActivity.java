0cd87be258474f3c594825f217511d11ac5318f8
Perry Li <perry0406@hotmail.com>
2019-09-21 16:53:34 -0700

    added finished goodfoodsmarket

---
+++ GoodFoodsMarket/app/src/main/java/com/example/goodfoodsmarket/ProductDetailActivity.java
@@ -0,0 +1,40 @@
+package com.example.goodfoodsmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+        Toast toast = Toast.makeText(getApplicationContext(), "inside product detail!", Toast.LENGTH_LONG);
+        toast.show();
+
+
+        Bundle bundle = getIntent().getExtras();
+        ImageView imageView = findViewById(R.id.imageView1);
+        TextView nameView = findViewById(R.id.textName);
+        TextView categoryView = findViewById(R.id.textCategory);
+        TextView countryOfOriginView = findViewById(R.id.textCountryOfOrigin);
+        TextView unitView = findViewById(R.id.textUnit);
+        TextView priceView = findViewById(R.id.textPrice);
+
+        byte[] b = bundle.getByteArray("photo");
+        Bitmap bitmap = BitmapFactory.decodeByteArray(b, 0, b.length);
+
+        imageView.setImageBitmap(bitmap);
+        nameView.setText(bundle.getString("name"));
+        categoryView.setText(bundle.getString("category"));
+        countryOfOriginView.setText(bundle.getString("countryOfOrigin"));
+        unitView.setText(bundle.getString("unit"));
+        priceView.setText(String.valueOf(bundle.getFloat("price")));
+    }
+}
