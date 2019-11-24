7a7e4b38cf93356b3e3d98d6c83d5168e9962879
Perry Li <perry0406@hotmail.com>
2019-11-24 12:19:16 -0800

    Added math lab

---
+++ lab13/app/src/main/java/ca/bcit/ass2/lab13/App.java
@@ -0,0 +1,9 @@
+package ca.bcit.ass2.lab13;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    public static int SCORE = 0;
+    public static int TOTAL_QUESTIONS = 0;
+}
